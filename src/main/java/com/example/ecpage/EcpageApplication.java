01b9e953d20ba74d048cb7006586daa46f9c04ad
JANG-90 <wahall90@gmail.com>
2023-10-06 01:43:41 +0900

    10.6

---
+++ src/main/java/com/example/ecpage/EcpageApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcpageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcpageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ecpage/EcpageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcpageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
